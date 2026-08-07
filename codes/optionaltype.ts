99d6ec0dd8f996d15bf831025ab6ce1420e64b66
Tharun Kumar <22x51a3225@srecnandyal.edu.in>
2026-08-08 00:04:13 +0530

    Add files via upload

---
+++ codes/optionaltype.ts
@@ -0,0 +1,22 @@
+// Using optional as well
+
+type Ur = {
+    name: string;
+    count: number;
+    isFruit?:boolean;
+};
+
+let u11: Ur ={
+    name: "orange",
+    count: 10,
+    isFruit: true
+};
+let u12: Ur ={
+    name: "banana",
+    count: 25,
+    isFruit:true
+};
+let u13: Ur={
+    name: "tomato",
+    count: 100
+};
---
+++ codes/type.ts
@@ -0,0 +1,18 @@
+type User = {
+    name: string;
+    count: number;
+};
+
+let u1: User ={
+    name: "orange",
+    count: 10
+};
+let u2: User ={
+    name: "banana",
+    count: 25
+};
+let u3: User={
+    name: "Apple",
+    count: 100
+};
+
