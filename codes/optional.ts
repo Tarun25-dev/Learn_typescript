efb3a10ff54c54b734b072111516b5b6b1060ae7
Tharun Kumar <22x51a3225@srecnandyal.edu.in>
2026-08-08 00:03:15 +0530

    Add files via upload

---
+++ codes/optional.ts
@@ -0,0 +1,2 @@
+let user1: {name: string; age: number; isPractice?: boolean} = {name: "tharun", age: 25, isPractice: true};
+let user2: {name: string; age: number; isPractice?: boolean} = {name: "tharun", age: 25};
