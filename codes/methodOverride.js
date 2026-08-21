17ab1bf7f6e33bb58278a30a2c08261aadc6a876
Tharun Kumar <22x51a3225@srecnandyal.edu.in>
2026-08-21 22:54:46 +0530

    Add files via upload

---
+++ codes/methodOverride.js
@@ -0,0 +1,13 @@
+"use strict";
+class Animal {
+    speak() {
+        console.log("Animal makes a sound");
+    }
+}
+class Dog extends Animal {
+    speak() {
+        console.log("Dog barks");
+    }
+}
+const dog = new Dog();
+dog.speak();
---
+++ codes/methodOverride.ts
@@ -0,0 +1,17 @@
+class Animal{
+    speak(){
+        console.log("Animal makes a sound");
+    }
+}
+
+class Dog extends Animal{
+    speak(){
+        console.log("Dog barks");
+    }
+}
+
+const dog = new Dog();
+
+dog.speak();
+
+// Dog barks
