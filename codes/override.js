22a7a9d86ecb1b415535dd2d3edfd99fb68f992a
Tharun Kumar <22x51a3225@srecnandyal.edu.in>
2026-08-21 22:55:12 +0530

    Add files via upload

---
+++ codes/override.js
@@ -0,0 +1,22 @@
+"use strict";
+class Animal {
+    move() {
+        console.log("Animal is moving");
+    }
+}
+class Dog extends Animal {
+    move() {
+        console.log("Dog is running");
+    }
+}
+class Fish extends Animal {
+    move() {
+        console.log("Fish is swimming");
+    }
+}
+const dog = new Dog();
+const fish = new Fish();
+dog.move();
+fish.move();
+// Dog is running
+// Fish is swimming
---
+++ codes/override.ts
@@ -0,0 +1,26 @@
+class Animal{
+    move(): void{
+        console.log("Animal is moving");
+    }
+}
+
+class Dog extends Animal{
+    override move(): void{
+        console.log("Dog is running");
+    }
+}
+
+class Fish extends Animal{
+    override move(): void {
+        console.log("Fish is swimming");
+    }
+}
+
+const dog = new Dog();
+const  fish = new Fish();
+
+dog.move();
+fish.move();
+
+// Dog is running
+// Fish is swimming
