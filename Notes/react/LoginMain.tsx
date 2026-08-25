2002c3b5ff0284df2bc92a9f92ac3be89bca639b
Tharun Kumar <22x51a3225@srecnandyal.edu.in>
2026-08-25 21:28:37 +0530

    Add files via upload

---
+++ Notes/react/LoginMain.tsx
@@ -0,0 +1,10 @@
+function MainPage(){
+    return(
+        <main className="mx-auto max-w-4xl p-6">
+            <h1 className="text-3xl font-bold">Welcome!</h1>
+            <p  className="mt-2 text-grey-600">You are successfully logged in.</p>
+        </main>
+    );
+}
+
+export default MainPage;
